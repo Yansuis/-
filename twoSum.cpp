27755d8f698b62b20363d0b13a2c45676b9f72fe
Yansuis <32156875+Yansuis@users.noreply.github.com>
2018-04-12 16:40:45 +0800

    Create twoSum.cpp

---
+++ twoSum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int size = nums.size();
+        vector<int> index;
+        for(int i=0;i<size;++i)
+            for(int j=i+1;j<size;++j)
+                if(nums[i]+nums[j]==target)
+                {
+                    index.push_back(i);
+                    index.push_back(j);
+                    return index;
+                }
+        return index;
+    }
+};
